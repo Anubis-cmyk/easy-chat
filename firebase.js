e7df6137d60561dad1c859c6f03a01ba7924eff5
Anubis-cmyk <it19099132@my.sliit.lk>
2021-11-24 19:18:08 +0530

    change firebase db

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth'; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyByOYmUX_3xrU7p9LnDBFvQCJ2r6ohFMJA",
-    authDomain: "easy-chat-3c8cc.firebaseapp.com",
-    databaseURL: "https://easy-chat-3c8cc-default-rtdb.firebaseio.com",
-    projectId: "easy-chat-3c8cc",
-    storageBucket: "easy-chat-3c8cc.appspot.com",
-    messagingSenderId: "991420514795",
-    appId: "1:991420514795:web:13d673e674ecd5011159ae",
-    measurementId: "G-701S0SFY4R" 
+    apiKey: "AIzaSyAEQXEzU4rZ1RqudPpPLkCg4fRXKyhC3Xw",
+    authDomain: "easy-chat-3c67b.firebaseapp.com",
+    databaseURL: "https://easy-chat-3c67b-default-rtdb.firebaseio.com",
+    projectId: "easy-chat-3c67b",
+    storageBucket: "easy-chat-3c67b.appspot.com",
+    messagingSenderId: "440698491831",
+    appId: "1:440698491831:web:7447b0feece29317cb8e32",
+    measurementId: "G-GNQJTVCY4N"
   };
 let app;
 
